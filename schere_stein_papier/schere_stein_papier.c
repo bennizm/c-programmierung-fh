6f5c1f5af10b60c362367e138b0dc87d233667cf
Benjamin Zimmermann <m.benjamin.zimmermann@gmail.com>
2025-09-14 23:35:55 +0200

    schere-stein-papier dokumentation angefangen

--- schere_stein_papier/schere_stein_papier.c
+++ schere_stein_papier/schere_stein_papier.c
@@ -1,22 +1,49 @@
-
-/**
- * Schere-Stein-Papüüür
+/*
+ * Schere-Stein-Papüüüür
  */
 
+/**
+ * hier werden die libraries includiert die benötigt werden.
+ * <stdio.h> braucht man für printf und scanf.
+ * <stdlib.h> braucht man für die methode rand() zur zufallsgenerierung.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * hier werden die konstanten definiert.
+ * schere "SCISSOR", stein "ROCK" und papier "PAPER" denen jeweils "0", "1" und "2" zugewiesen werden.
+ * das wird gemacht, damit jeder der drei formen quasi ein einheitlicher wert bzw "id" zugewiesen wird,
+ * da man im programmcode ja nicht mit den worten scissor und so arbeiten kann.
+ */
 #define SCISSOR 0
 #define ROCK 1
 #define PAPER 2
 
+/**
+ * hier wird eine zufällige zahl zwischen 0 und 2 generiert mittels rand() und einer art "formel" die
+ * die zufallszahlen eingrenzt, da die methode rand() ansich nicht über parameter definiert werden kann.
+ * sie gibt nämlich eine zahl zwischen 0 und RAND_MAX zurück.
+ * int M bildet das minimum
+ * int N bildet das maximum
+ * @return eine zufallszahl zwischen 0 und 2, die der computer "spielt"
+ */
 int computerChoiceRandom() {
     int M = 0;
     int N = 2;
     return M + rand() / (RAND_MAX / (N - M + 1) + 1);
 }
 
+/**
+ * hier findet das eigentliche spiel statt.
+ * es handelt sich dabei um eine rekursive methode, die solang wiederholt wird, solange der spieler
+ * eine zahl eingibt, die nicht -1 ist, dann wird das spiel beendet und damit das programm.
+ * genauere erklärungen folgen.
+ * @param wins die anzahl der gewinne des spielers
+ * @param losses die anzahl wie oft der spieler verkackt hat
+ */
 void ssp(int wins, int losses) {
+    //TODO: Erklärung für alles folgende bin schon zu müde heut DAMN
     const char * literals[] = {
         "Schere",
         "Stein",
