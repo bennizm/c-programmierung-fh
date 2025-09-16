00a2267b2091d64c233b93479f2b1622d42020ee
Benjamin Zimmermann <m.benjamin.zimmermann@gmail.com>
2025-09-16 20:23:21 +0200

    notizen aktualisiert

--- mitschrift/notes.c
+++ mitschrift/notes.c
@@ -1,9 +1,22 @@
-/**
+/*
+ * COMPILER INSTALLIEREN
+ * 1) unter windoof
+ *    a) WSL (windows subsystem for linux) installieren
+ *       "wsl --install" auf der Windows-Powershell eingeben
+ *    b) im Microsoft-Store "wsl" eingeben und Ubuntu herunterladen
+ *    c) compiler herunterladen
+ *       c1) "sudo apt-get install gcc-mingw-w64" um in windows executables ".exe" zu kompilieren
+ *       c2) "sudo apt-get install gcc" um in linux executables zu kompilieren
+ *
+ *   compiler müssten nun funktionieren (siehe nächsten Kommentar "KOMPILIEREN"
+ */
+
+/*
+ * KOMPILIERN
  * Compile C-File with CLion-Compiler option via GUI
  * or via WSL commands:
  * for windows ".exe" files: "x86_64-w64-mingw32-gcc -Wall -o <example>.exe <example>.c"
  * for linux executable files: "gcc -Wall -o <example> <example>.c"
- * @return 0
  */
 int main() {
     return 0;
