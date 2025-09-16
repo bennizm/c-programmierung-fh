af956f2769d213f0772349d0e4753543bcae0990
Benjamin Zimmermann <m.benjamin.zimmermann@gmail.com>
2025-09-16 18:56:01 +0200

    tippfehler ausgebessert

--- mitschrift/notes.c
+++ mitschrift/notes.c
@@ -1,5 +1,5 @@
 /**
- * Compile C-File with CLion-Compile option via GUI
+ * Compile C-File with CLion-Compiler option via GUI
  * or via WSL commands:
  * for windows ".exe" files: "x86_64-w64-mingw32-gcc -Wall -o <example>.exe <example>.c"
  * for linux executable files: "gcc -Wall -o <example> <example>.c"
